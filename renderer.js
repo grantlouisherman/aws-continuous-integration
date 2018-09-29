6785403028a14ec2b141caf07bef618ac8ee725a
Grant Herman <grantlouisherman041@gmail.com>
2018-09-29 14:45:59 -0700

    feature():adding renderer

---
+++ renderer.js
@@ -0,0 +1,10 @@
+
+$(document).ready(() => {
+  const button = document.getElementById("SUBMIT");
+  $("#SUBMIT").click(() => {
+    console.log("HI GRANT")
+        // $.ajax({url: "demo_test.txt", success: function(result){
+        //     $("#div1").html(result);
+        // }});
+    });
+})
