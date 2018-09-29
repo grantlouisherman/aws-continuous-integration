78ebf8bef283d09eea13e46d063a727e6d2b71ad
Grant Herman <grantlouisherman041@gmail.com>
2018-09-29 13:49:27 -0700

    updating to hide keys

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 const AWS = require('aws-sdk'),
     fs = require('fs');
-
+const keys =  require('./keys.json')
 const FOLDER = `./test`;
 const BUCKET_NAME = 'searchapp';
-const IAM_USER_KEY = 'AKIAIEVMUNOTWVRCEAUA';
-const IAM_USER_SECRET = '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';
 
 function uploadToS3(name, file) {
 fs.readFile(file, function (err, data) {
   const base64data = new Buffer(data, 'binary');
   let s3bucket = new AWS.S3({
-     accessKeyId: IAM_USER_KEY,
-     secretAccessKey: IAM_USER_SECRET,
+     accessKeyId: keys.IAM_USER_KEY,
+     secretAccessKey: keys.IAM_USER_SECRET,
      Bucket: BUCKET_NAME,
    });
     const params = {
