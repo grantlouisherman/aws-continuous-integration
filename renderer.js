d4909f3ebee9ed3ff07cc00625ed72e2cb37c4cb
Grant Herman <grantlouisherman041@gmail.com>
2018-11-30 09:31:08 -0800

    feature(): moving to an electron centric application. Still have not gotten unwatch to work.

--- renderer.js
+++ renderer.js
@@ -1,33 +1,14 @@
+const ipc = require('electron').ipcRenderer;
 
-$(document).ready(() => {
+const { submitClickCB, unWatchFileCB } = require('./utils');
 
-  const successful = document.getElementById("successful");
-  const failure = document.getElementById("failure");
-  $(successful).hide();
-  $(failure).hide()
-  const button = document.getElementById("SUBMIT");
-  const BUCKET_NAME = document.getElementById("BUCKET_NAME");
-  const IAM_USER_KEY = document.getElementById("IAM_USER_KEY");
-  const IAM_USER_SECRET = document.getElementById("IAM_USER_SECRET");
-  const FOLDER = document.getElementById("FOLDER");
-
-  $("#SUBMIT").click(() => {
-        $.ajax({
-          type:'GET',
-          url: "http://localhost:4000/aws",
-          headers:{
-            "FOLDER_LOCATION":FOLDER.value,
-            "BUCKET_NAME":BUCKET_NAME.value,
-            "IAM_USER_KEY":IAM_USER_KEY.value,
-            "IAM_USER_SECRET":IAM_USER_SECRET.value
-          },
-          cache:true,
-          success: msg => {
-            $(successful).show();
-          },
-          error: msg => {
-            $(failure).show();
-          }
-        })
-    });
-})
+const BUCKET_NAME = document.getElementById("BUCKET_NAME");
+const IAM_USER_KEY = document.getElementById("IAM_USER_KEY");
+const IAM_USER_SECRET = document.getElementById("IAM_USER_SECRET");
+const FOLDER = document.getElementById("FOLDER");
+document.getElementById('SUBMIT').addEventListener('click', () => {
+  ipc.once('submitReply', function(event, response){
+    submitClickCB.call(this,FOLDER.value, IAM_USER_KEY.value, IAM_USER_SECRET.value, BUCKET_NAME.value )
+   });
+   ipc.send('submitAction', 'clicked');
+});
